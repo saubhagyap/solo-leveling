61da92e9f3e7fba92a4e4c42fe24bedf987a2a25
Saubhagya Pandey <saubhagyapandey20@gmail.com>
2022-09-09 03:52:15 +0530

    changed Image

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,7 @@ void main() {
           backgroundColor: Color.fromARGB(255, 55, 51, 51),
         ),
         body: Center(
-          child: Image(
-              image: NetworkImage(
-                  'https://w0.peakpx.com/wallpaper/884/72/HD-wallpaper-solo-leveling-manga-scan-solo-leveling.jpg')),
+          child: Image(image: AssetImage('images/solo_leveling.jpg')),
         ),
       ),
     ),
